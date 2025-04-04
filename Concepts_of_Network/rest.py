669d950b5abed2ab9d51847033c0eb2518ba40c5
olga-sdev <olga.github@airpost.net>
2025-04-04 21:54:31 +0300

    Create rest.py

---
+++ Concepts_of_Network/rest.py
@@ -0,0 +1,8 @@
+"""
+REST-> it's an acronym:
+
+REpresentational -> machinery stores, transmits and receives representations(DATA or states) are retained inside the system and presented to the users;
+State -> CHANGES (transition) of properties' VALUES
+Transfer -> network (carrier) allows to transmit states' data TO and FROM the server.
+
+"""
