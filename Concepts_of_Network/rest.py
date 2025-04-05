c0f9db05d28ef00a6e059cdd2701dff0fe22e994
olga-sdev <olga.github@airpost.net>
2025-04-05 13:14:34 +0300

    Update and rename rest.py to rest.md

--- Concepts_of_Network/rest.py
+++
@@ -1,8 +0,0 @@
-"""
-REST-> it's an acronym:
-
-REpresentational -> machinery stores, transmits and receives representations(DATA or states) are retained inside the system and presented to the users;
-State -> CHANGES (transition) of properties' VALUES
-Transfer -> network (carrier) allows to transmit states' data TO and FROM the server.
-
-"""
