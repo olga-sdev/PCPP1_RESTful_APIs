33bbb2c754511e5db617422163f3f90e92c38488
olga-sdev <olga.github@airpost.net>
2025-04-05 15:05:27 +0300

    Update create_socket.py

--- Sockets_in_Python/create_socket.py
+++ Sockets_in_Python/create_socket.py
@@ -28,13 +28,13 @@ close() -> closing a socket releases the resources associated with it, ensuring 
 import socket
 
 
-server_addr = input("Input server you want to connect to: ")
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((server_addr, 80))
-data = b"GET / HTTP/1.1\r\nHost: " +
+server_addr = 'www.google.com'
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((server_addr, 80))
+sock.send(b"GET / HTTP/1.1\r\nHost: " +
           bytes(server_addr, "utf8") +
-          b"\r\nConnection: close\r\n\r\n"
-s.send(data)
+          b"\r\nConnection: close\r\n\r\n")
 reply = sock.recv(10000)
 sock.shutdown(socket.SHUT_RDWR)
 sock.close()
+print(repr(reply))
