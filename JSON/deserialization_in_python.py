6945561ae96642b9f503296436b664933d474c85
olga-sdev <olga.github@airpost.net>
2025-04-10 23:56:34 +0300

    Update deserialization_in_python.py

--- JSON/deserialization_in_python.py
+++ JSON/deserialization_in_python.py
@@ -11,3 +11,32 @@ print(book)
 
 # <class 'str'>
 # "Python Tricks" by Dan Bader
+
+
+# Example for deserialization
+class Vehicle:
+    def __init__(self, name, width, height, weight):
+        self.name = name
+        self.width = width
+        self.height = height
+        self.weight = weight
+
+
+def encode_vehicle_to_dict(obj):
+    if isinstance(obj, Vehicle):
+        return obj.__dict__
+    raise TypeError(f'Object of type {type(obj).__name__} is not JSON serializable')
+
+
+def decode_vehicle(obj):
+    return Vehicle(obj['name'], obj['width'], obj['height'], obj['weight'])
+
+
+bus = Vehicle('Red Tourist Bus', 4, 5, 20)
+json_bus = json.dumps(bus, default=encode_vehicle_to_dict)
+new_bus = json.loads(json_bus, object_hook=decode_vehicle)
+print(type(new_bus))
+print(new_bus.__dict__)
+
+# <class '__main__.Vehicle'>
+# {'name': 'Red Tourist Bus', 'width': 4, 'height': 5, 'weight': 20}
